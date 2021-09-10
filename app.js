f1f62001d04c90fe18a20a74cac04c4140e077b9
francoterragna <francoterragna@hotmail.com>
2021-09-09 22:59:17 -0300

    paginas login

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const puerto = 3000;
 app.listen(puerto, () => console.log('Servidor corriendo en el puerto 3000'));
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')));
+app.get('/registro', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html')))
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, '/views/login.html')))
