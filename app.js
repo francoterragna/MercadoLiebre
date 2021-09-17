eb70b3e6f2ae3ebaa7d7b9edf3398cc46b7cd9a9
francoterragna <francoterragna@hotmail.com>
2021-09-16 22:19:16 -0300

    Procfile heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require('path');
 const puerto = 3000;
-app.listen(puerto, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || puerto, () => console.log('Servidor corriendo en el puerto 3000'));
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')));
