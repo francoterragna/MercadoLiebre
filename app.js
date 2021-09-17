58741d9e00b864345dabae2eb2593b038d5bb12a
francoterragna <francoterragna@hotmail.com>
2021-09-16 22:32:19 -0300

    cambios en app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require ('express');
 const app = express();
 const path = require('path');
-const puerto = 3000;
-app.listen(process.env.PORT || puerto, () => console.log('Servidor corriendo en el puerto 3000'));
+const puerto = process.env.PORT || 3000;
+app.listen(puerto, () => console.log('Servidor corriendo en el puerto 3000'));
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')));
